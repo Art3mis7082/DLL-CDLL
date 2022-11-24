d1aaa51fab013f201c5ef3f7cb67ff099f33b7d3
Beatriz Almaraz <90168869+Art3mis7082@users.noreply.github.com>
2022-11-23 22:21:43 -0600

    Update CDLL.h
    Modificacion 1

--- cdll/CDLL.h
+++ cdll/CDLL.h
@@ -1,22 +1,3 @@
-/*Copyright (C) 
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
- * 2022 - francisco dot rodriguez at ingenieria dot unam dot edu
- */
-
 
 #ifndef  CDLL_INC
 #define  CDLL_INC
@@ -35,15 +16,22 @@
 
 #include "defs.h"
 
+typedef struct Song
+{
+   char nombre;
+   char autor;
+   int duracion;
+}Song;
+
 typedef struct Node
 {
-	int datos;
+	Song* cancion;
 
 	struct Node* next;
 	struct Node* prev;
 } Node;
 
-typedef struct
+typedef struct 
 {
 	Node* first;
 	Node* last;
@@ -58,9 +46,9 @@ void CDLL_Delete( CDLL** this );
 int CDLL_Front( CDLL* this );
 int CDLL_Back( CDLL* this );
 
-void CDLL_Push_front( CDLL* this, int item );
-void CDLL_Push_back( CDLL* this, int item );
-void CDLL_Insert( CDLL* this, int item );
+void CDLL_Push_front( CDLL* this, int time, char name, char author);
+void CDLL_Push_back( CDLL* this, int time, char name, char author );
+void CDLL_Insert( CDLL* this, int time, char name, char author );
 
 void CDLL_Pop_front( CDLL* this );
 void CDLL_Pop_back( CDLL* this );
@@ -86,9 +74,10 @@ size_t CDLL_Len( CDLL* this );
 void CDLL_PrintStructure( CDLL* this );
 
 // avanzadas:
-void CDLL_For_each( CDLL* this, void (*p_fn)( int item ) );
+void CDLL_For_each( CDLL* this, void (*p_fn)( int time, char name, char author ) );
 Node* CDLL_Find_if( CDLL* this, bool (*p_fn)( int, int ), int key );
 size_t CDLL_Remove_if( CDLL* this, bool (*cmp)( int x, int y ), int key );
 
 
 #endif   /* ----- #ifndef CDLL_INC  ----- */
+
